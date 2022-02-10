a362bd04dcead4eef84c8a3a03977a442c86118a
LiuYang <30458803+lyonliuyang@users.noreply.github.com>
2022-02-10 10:14:01 +0100

    Update ortholog_download.py

--- ortholog_download.py
+++ ortholog_download.py
@@ -1,41 +1,30 @@
 import requests
-from multiprocessing import Pool
 import os
+import time
 
-def main(i):
-    error_log = open('/mnt/sdb1/home/liuyang/Pseudomonas_database/pseudocap/PAO1_Ortholog/error.log','a+')
-    _id_ = i.rstrip()
-    url = ('http://pseudomonas.com/orthologs/list?format=tab&extension=tab&id=%s'%_id_)
+def arg():
+    pass
+    arg.workdir = '/data/Lyon/project2/orthology_pao1_fromdatabase/'
+    return
 
-    out_path = ("/mnt/sdb1/home/liuyang/Pseudomonas_database/pseudocap/PAO1_Ortholog/%s.tab"%_id_)
-    if os.path.exists(out_path) == True:
-        return
+def main():
+    arg()
+    os.system("""cut -f5 '%s/Pseudomonas_aeruginosa_PAO1_107.tsv' | sed 's/PGD//g' > %s/PAO1_gene_id.txt"""%(arg.workdir,arg.workdir))
+    with open('%s/PAO1_gene_id.txt'%arg.workdir) as f:
+        PAO1_id = f.readlines()
     
-    try:
-        r = requests.get(url,timeout=50)
-        if(len(r.content)!=17339): # length of error code
-            with open(out_path,"wb") as code:
-                code.write(r.content)
-                error_log.close()
-                return
-        else:
-            error_log.write(_id_ + '\t' + 'else' +'\n')
-    except OSError:
-        error_log.write(_id_ + '\t' + 'except' +'\n')
-        pass
+    script_log = open('%s/download.sh'%arg.workdir,'w')
+    for i in PAO1_id:
+        
+        _id_ = i.rstrip()
+        if 'Gene ID' in _id_:continue
+        url = ('http://pseudomonas.com/orthologs/list?format=tab&extension=tab&id=%s'%_id_)
 
-    finally:
-        error_log.write(_id_ + '\t' + 'null' +'\n')
-        error_log.close()
-        return
+        script_log.write("""wget '%s' -O %s/orthology_group_pao1_tab/%s.tab\n"""%(url,arg.workdir,_id_))
+        script_log.write('sleep 1s \n')
+    script_log.close()
+    return
 
 if __name__ == '__main__':
-    os.system("cut -f5 /mnt/sdb1/home/liuyang/Pseudomonas_database/pseudocap/PAO1_Ortholog/Pseudomonas_aeruginosa_PAO1_107.tsv | sed 's/PGD//g' > /mnt/sdb1/home/liuyang/Pseudomonas_database/pseudocap/PAO1_Ortholog/PAO1_gene_id.txt ")
-    with open('/mnt/sdb1/home/liuyang/Pseudomonas_database/pseudocap/PAO1_Ortholog/PAO1_gene_id.txt') as f:
-        PAO1_id = f.readlines()
-    #p = Pool(18)
-    for i in PAO1_id:
-        main(i)
-        #p.apply_async(main,args=(i,))
-    #p.close()
-    #p.join()
+    main()
+    #os.system("""'nohup bash %s/download.sh &'%arg.workdir""")
